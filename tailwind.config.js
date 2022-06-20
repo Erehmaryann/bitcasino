90686c56f1624203054dee5eaebe57bc0178b839
erehmaryann <edwardmaryann09@gmail.com>
2022-06-20 15:26:27 -0700

    updated the config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,17 @@ module.exports = {
     extend: {
       colors: {
         'primary': '#2a0a4a',
-      }
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          lg: '4rem',
+          xl: '5rem',
+          '2xl': '6rem',
+        },
+      },
     },
   },
   plugins: [],
